253c733928d7798f53bb6d512bdd9f67eedb97d9
Felippe Ribon <henryf.ribon@gmail.com>
2026-04-26 17:28:39 -0300

    atualizei coisa pra caramba, mas em resumo deu pra colocar a interação com o usuário, verificação se o valor está correto ou errado e o looping está funcionando, agora só falta o sistema de pontuação para acabar

--- fase1.projeto.c
+++ fase1.projeto.c
@@ -24,32 +24,79 @@ Separação de responsabilidades:
 #include <stdlib.h>
 #include <time.h>
 
-void solicitar_dificuldade();
-void calculo_facil();
-void calculo_medio();
+void loop_interacao_usuario();
+int calculo_facil();
+int calculo_medio();
 int calculo_dificil();
+int resposta_usuario();
+void verificar_resposta(int resposta_usuario, int resposta_correta);
 
 int main()
 {
-  // calculo_facil();
-  // calculo_medio();
-  calculo_dificil();
+  srand(time(NULL)); // Inicializa o gerador de números aleatórios apenas uma vez
+
+  loop_interacao_usuario();
 
   return 0;
 }
 
-void solicitar_dificuldade()
+void loop_interacao_usuario()
 {
-  int dificuldade;
-  printf("Escolha o nível de dificuldade (1 - Fácil, 2 - Médio, 3 - Difícil): ");
-  scanf("%d", &dificuldade); // Aqui você pode adicionar lógica para validar a entrada do usuário e gerar cálculos com base na dificuldade escolhida.
+  while (1)
+  {
+    int dificuldade;
+
+    printf("Escolha o nível de dificuldade (0 - Sair,1 - Fácil, 2 - Médio, 3 - Difícil): ");
+    scanf("%d", &dificuldade);
+
+    switch (dificuldade)
+    {
+    case 0:
+      printf("Encerrando o jogo. Obrigado por jogar!\n");
+      printf("\n");
+      return; // Encerra a função
+
+    case 1:
+    {
+      int resposta_correta = calculo_facil();
+      int resposta = resposta_usuario();
+
+      verificar_resposta(resposta, resposta_correta);
+      printf("\n");
+      break;
+    }
+
+    case 2:
+    {
+      int resposta_correta = calculo_medio();
+      int resposta = resposta_usuario();
+
+      verificar_resposta(resposta, resposta_correta);
+      printf("\n");
+      break;
+    }
+
+    case 3:
+    {
+      int resposta_correta = calculo_dificil();
+      int resposta = resposta_usuario();
+
+      verificar_resposta(resposta, resposta_correta);
+      printf("\n");
+      break;
+    }
+
+    default:
+      printf("Opção inválida! Por favor, escolha um nível de dificuldade válido.\n");
+      printf("\n");
+      break;
+    }
+  }
 }
 
 // Vou trabalhar com um calculo simples de adição, subtração ou multiplicação com números pequenos.
-void calculo_facil()
+int calculo_facil()
 {
-  srand(time(NULL)); // Inicializa o gerador de números aleatórios
-
   int definicao_operacao = rand() % 3; // Gera um número aleatório entre 0 e 2 para escolher a operação
   int num1 = 0, num2 = 0, resultado = 0;
 
@@ -61,7 +108,7 @@ void calculo_facil()
   case 0:
     resultado = num1 + num2; // Calcula a soma
     printf("%d + %d = ?", num1, num2);
-    break;
+    return resultado; // Retorna o resultado
 
   case 1:
     // Coloquei esse if para evitar uma subtração que de negativo
@@ -69,27 +116,27 @@ void calculo_facil()
     {
       resultado = num2 - num1; // Calcula a subtração
       printf("%d - %d = ?", num2, num1);
+      return resultado; // Retorna o resultado
     }
     else
     {
       resultado = num1 - num2; // Calcula a subtração
       printf("%d - %d = ?", num1, num2);
+      return resultado; // Retorna o resultado
     }
-    break;
 
   case 2:
     resultado = num1 * num2; // Calcula a multiplicação
     printf("%d * %d = ?", num1, num2);
-    break;
+    return resultado; // Retorna o resultado
 
   default:
-    break;
+    return 0;
   }
 }
 
-void calculo_medio()
+int calculo_medio()
 {
-  srand(time(NULL)); // Inicializa o gerador de números aleatórios
   int operacao1 = 1, operacao2 = 1;
   int num1 = 0, num2 = 0, num3 = 0, opr_num1_num2 = 0, resultado = 0;
 
@@ -112,7 +159,6 @@ void calculo_medio()
     // Como é de nivel médio, nao vou ajustar para ficar sem negativos
     opr_num1_num2 = num1 - num2; // Calcula a subtração
     printf("Quanto é %d - %d", num1, num2);
-
     break;
 
   case 2:
@@ -121,7 +167,7 @@ void calculo_medio()
     break;
 
   default:
-    break;
+    return 0;
   }
 
   // Aqui vou definir qual vai ser a segunda operação
@@ -130,27 +176,26 @@ void calculo_medio()
   case 0:
     resultado = opr_num1_num2 + num3; // Calcula a soma
     printf(" + %d = ?", num3);
-    break;
+    return resultado; // Retorna o resultado
 
   case 1:
     // Como é de nivel médio, nao vou ajustar para ficar sem negativos
     resultado = opr_num1_num2 - num3; // Calcula a subtração
     printf(" - %d = ?", num3);
-    break;
+    return resultado; // Retorna o resultado
 
   case 2:
     resultado = opr_num1_num2 * num3; // Calcula a multiplicação
     printf(" * %d = ?", num3);
-    break;
+    return resultado; // Retorna o resultado
 
   default:
-    break;
+    return 0;
   }
 }
 
 int calculo_dificil()
 {
-  srand(time(NULL)); // Inicializa o gerador de números aleatórios
   int operacao1 = 1, operacao2 = 1, operacao3 = 1;
   int num1 = 0, num2 = 0, num3 = 0, num4 = 0, opr_num1_num2 = 0, opr_num3_num4 = 0, resultado = 0;
 
@@ -158,80 +203,96 @@ int calculo_dificil()
   operacao2 = rand() % 3; // Gera um número aleatório entre 0 e 2 para escolher a operação
   operacao3 = rand() % 3; // Gera um número aleatório entre 0 e 2 para escolher a operação
 
-  num1 = (rand() % 100) + 1; // Gera um número aleatório entre 1 e 100
-  num2 = (rand() % 100) + 1; // Gera um número aleatório entre 1 e 100
-  num3 = (rand() % 100) + 1; // Gera um número aleatório entre 1 e 100
-  num4 = (rand() % 100) + 1; // Gera um número aleatório entre 1 e 100
+  num1 = (rand() % 100) + 1;
+  num2 = (rand() % 100) + 1;
+  num3 = (rand() % 100) + 1;
+  num4 = (rand() % 100) + 1;
 
   // Aqui vou definir qual vai ser a primeira operação
   switch (operacao1)
   {
   case 0:
-    opr_num1_num2 = num1 + num2; // Calcula a soma
+    opr_num1_num2 = num1 + num2;
     printf("Quanto é %d + %d", num1, num2);
     break;
 
   case 1:
-    // Como é de nivel médio, nao vou ajustar para ficar sem negativos
-    opr_num1_num2 = num1 - num2; // Calcula a subtração
+    opr_num1_num2 = num1 - num2;
     printf("Quanto é %d - %d", num1, num2);
-
     break;
 
   case 2:
-    opr_num1_num2 = num1 * num2; // Calcula a multiplicação
+    opr_num1_num2 = num1 * num2;
     printf("Quanto é %d * %d", num1, num2);
     break;
 
   default:
-    break;
+    return 0;
   }
 
   // Aqui vou definir qual vai ser a segunda operação
   switch (operacao2)
   {
   case 0:
-    resultado = opr_num1_num2 + num3; // Calcula a soma
+    opr_num3_num4 = opr_num1_num2 + num3;
     printf(" + %d ", num3);
     break;
 
   case 1:
-    // Como é de nivel médio, nao vou ajustar para ficar sem negativos
-    resultado = opr_num1_num2 - num3; // Calcula a subtração
+    opr_num3_num4 = opr_num1_num2 - num3;
     printf(" - %d ", num3);
     break;
 
   case 2:
-    resultado = opr_num1_num2 * num3; // Calcula a multiplicação
+    opr_num3_num4 = opr_num1_num2 * num3;
     printf(" * %d ", num3);
     break;
 
   default:
-    break;
+    return 0;
   }
 
   // Aqui vou definir qual vai ser a terceira operação
   switch (operacao3)
   {
   case 0:
-    resultado = resultado + num4; // Calcula a soma
+    resultado = opr_num3_num4 + num4;
     printf(" + %d = ?", num4);
-    break;
+    return resultado;
 
   case 1:
-    // Como é de nivel médio, nao vou ajustar para ficar sem negativos
-    resultado = resultado - num4; // Calcula a subtração
+    resultado = opr_num3_num4 - num4;
     printf(" - %d = ?", num4);
-    break;
+    return resultado;
 
   case 2:
-    resultado = resultado * num4; // Calcula a multiplicação
+    resultado = opr_num3_num4 * num4;
     printf(" * %d = ?", num4);
-    break;
+    return resultado;
 
   default:
-    break;
+    return 0;
   }
+}
 
-  return 0; // Retorna o resultado do cálculo difícil
+// Função para solicitar a resposta do usuário
+int resposta_usuario()
+{
+  int resposta_usuario;
+  printf("\nDigite sua resposta: ");
+  scanf("%d", &resposta_usuario);
+  return resposta_usuario;
+}
+
+// Função para verificar a resposta do usuário
+void verificar_resposta(int resposta_usuario, int resposta_correta)
+{
+  if (resposta_usuario == resposta_correta)
+  {
+    printf("\nResposta correta! Você ganhou 1 ponto.\n");
+  }
+  else
+  {
+    printf("\nResposta incorreta! A resposta correta era: %d\n", resposta_correta);
+  }
 }
